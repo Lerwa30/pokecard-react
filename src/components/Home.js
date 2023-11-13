d2477e4035487a4551257992418c7d6856190424
Leroy <leroypawlick@gmail.com>
2023-11-13 10:39:58 -0700

    added api call to retrive cards and log data

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Searchbar from "./Searchbar";
 
 const Home = () => {
-    return (
-        <div>
-            Welcome to PokeCard Finder!
-            <Searchbar></Searchbar>
-        </div>
-    )
-}
+  const [cards, setCards] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("https://api.pokemontcg.io/v2/cards?q=set.id:sv3pt5", {
+        headers: {
+          "X-Api-Key": "12703c60-623a-4f2a-8c5b-582d91c84c5f",
+        },
+      })
+      .then((res) => {
+        console.log(res.data.data);
+      });
+  }, []);
+
+  return (
+    <div>
+      Welcome to PokeCard Finder!
+      <Searchbar></Searchbar>
+    </div>
+  );
+};
 
 export default Home;
