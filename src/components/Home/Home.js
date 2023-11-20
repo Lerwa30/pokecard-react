4d3a0b2e5b37f0073bda1f1f9dba3a5a01b37f01
Leroy <leroypawlick@gmail.com>
2023-11-20 15:25:42 -0700

    simplieifed fetch requests, eliminated duplicates in results

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,67 +6,66 @@ import Searchbar from "./Searchbar";
 import classes from "./Home.module.css";
 import pic from "../assets/pokecard.webp";
 
-
 const Home = () => {
   const [cards, setCards] = useState([]);
+
+  const fetchSets = async (setId) => {
+    try {
+      const response = await fetch(
+        `https://api.pokemontcg.io/v2/cards?q=set.id:${setId}`,
+        {
+          method: "GET",
+          headers: {
+            "X-Api-Key": "12703c60-623a-4f2a-8c5b-582d91c84c5f",
+          },
+        }
+      );
+
+      const data = await response.json();
+      setCards((prevCards) => {
+        const newCards = data.data.filter(
+          (newCard) => !prevCards.some((card) => card.id === newCard.id)
+        );
+        return [...prevCards, ...newCards];
+      });
+      console.log(data);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // First API call
-        const response1 = await fetch(
-          "https://api.pokemontcg.io/v2/cards?q=set.id:sv3pt5",
-          {
-            method: "GET",
-            headers: {
-              "X-Api-Key": "12703c60-623a-4f2a-8c5b-582d91c84c5f",
-            },
-          }
-        );
-
-        const data1 = await response1.json();
-        setCards(data1.data);
-
-        console.log(data1);
-
-        // Second API call
-        const response2 = await fetch(
-          "https://api.pokemontcg.io/v2/cards?q=set.id:base4",
-          {
-            method: "GET",
-            headers: {
-              "X-Api-Key": "12703c60-623a-4f2a-8c5b-582d91c84c5f",
-            },
-          }
-        );
-
-        const data2 = await response2.json();
-        setCards((prevCards) => [...prevCards, ...data2.data]);
-        console.log(data2);
+        await fetchSets("sv3pt5");
+        await fetchSets("base4");
+        await fetchSets("swsh12pt5");
+        await fetchSets("swsh12pt5gg")
       } catch (error) {
-        console.error("Error during data fetching:", error);
+        console.log("error", error);
       }
     };
 
     fetchData();
   }, []);
 
-  return ( <>
-    <div className={classes.home}>
-      <div
-     className={classes.pic}
-     style={{
-       background: `linear-gradient(
+  return (
+    <>
+      <div className={classes.home}>
+        <div
+          className={classes.pic}
+          style={{
+            background: `linear-gradient(
      190deg,
      rgba(0, 0, 0, 0.3),
      rgba(0, 0, 0, 0.1)),
      url(${pic})`,
-     backgroundSize: 'cover'
-     }}
-   ></div>
-      <Searchbar cards={cards}></Searchbar>
-    </div>
-   </>
+            backgroundSize: "cover",
+          }}
+        ></div>
+        <Searchbar cards={cards}></Searchbar>
+      </div>
+    </>
   );
 };
 
